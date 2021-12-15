25edf830fce08c974e9e8d8c54506148b50f41c5
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-12-16 00:37:32 +0100

    fill up basic settings of all routes

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const hbs = require('express-handlebars');
+const { cookieConfigRouter } = require('./routes/cookie-config');
+const { homeRouter } = require('./routes/home');
+const { orderRouter } = require('./routes/order');
 
 const app = express();
 app.use(express.static('public'));
@@ -11,5 +14,8 @@ app.set('view engine', '.hbs');
 app.get('/', (req, res) => {
     res.send('ok');
 });
+app.use('/', homeRouter);
+app.use('/cookie-config', cookieConfigRouter);
+app.use('/order', orderRouter);
 
 app.listen(3000, 'localhost');
---
+++ routes/cookie-config.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const cookieConfigRouter = express.Router();
+
+cookieConfigRouter;
+
+module.exports = {
+    cookieConfigRouter,
+};
