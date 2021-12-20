1a89db7c7ffab8dfef47560fa074eafea96a579d
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-12-21 00:32:12 +0100

    add a functionality in order to switch between choosed type of cookie and display right info

--- routes/cookie-config.js
+++ routes/cookie-config.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const cookieConfigRouter = express.Router();
 
-cookieConfigRouter.get('/', (req, res) => {
-    res.render('cookie-config/index');
+cookieConfigRouter.get('/select-type/:type', (req, res) => {
+    const { type } = req.params;
+    res.cookie('cookie-type', type).render('cookie-config/selected-type', {
+        type,
+    });
 });
 
 module.exports = {
