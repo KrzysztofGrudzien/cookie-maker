6cd2085d82d2403e6409141fac714a32ab958f03
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-12-20 19:09:36 +0100

    create usefull data

---
+++ data/cookies-data.js
@@ -0,0 +1,18 @@
+const COOKIES_TYPES = {
+    brown: 10,
+    light: 8,
+    dark: 12,
+};
+
+const COOKIES_ADDONS = {
+    honey: 10,
+    coconut: 8,
+    strawberries: 12,
+    sprinkles: 12,
+    candy: 12,
+};
+
+module.exports = {
+    COOKIES_ADDONS,
+    COOKIES_TYPES,
+};
