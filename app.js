282703ae111535839de73ca4c859300862ad3c9e
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-12-20 19:13:05 +0100

    setup handlebars helpers in the main file of app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const hbs = require('express-handlebars');
+const { handlebarsHelpers } = require('./handlebars-helpers/handlebars-helpers');
 const { cookieConfigRouter } = require('./routes/cookie-config');
 const { homeRouter } = require('./routes/home');
 const { orderRouter } = require('./routes/order');
@@ -9,7 +10,7 @@ const app = express();
 app.use(express.static('public'));
 app.use(cookieParser());
 
-app.engine('.hbs', hbs.engine({ extname: '.hbs' }));
+app.engine('.hbs', hbs.engine({ extname: '.hbs', helpers: handlebarsHelpers }));
 app.set('view engine', '.hbs');
 app.use('/', homeRouter);
 app.use('/cookie-config', cookieConfigRouter);
