1a816d057bbb4fe3756b2b758c7fe5b024b9da9a
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-12-20 19:15:29 +0100

    put in usefull data from cookies-data for Route - home.js

--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const homeRouter = express.Router();
+const { COOKIES_TYPES, COOKIES_ADDONS } = require('../data/cookies-data');
 
 homeRouter.get('/', (req, res) => {
     res.render('home/index', {
         cookie: {
-            type: 'brown-light',
-            addons: ['chocolate'],
+            type: 'light',
+            addons: ['coconut', 'strawberries', 'honey', 'candy', 'sprinkles'],
         },
+        types: Object.entries(COOKIES_TYPES),
+        addons: Object.entries(COOKIES_ADDONS),
     });
 });
 
