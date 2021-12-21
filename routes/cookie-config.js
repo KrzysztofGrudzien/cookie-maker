6f327b29f0b827b59c1a552bb964f56edd46b5b3
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-12-21 01:36:52 +0100

    create a functionality which asign cookie type in cookie browser

--- routes/cookie-config.js
+++ routes/cookie-config.js
@@ -3,7 +3,7 @@ const cookieConfigRouter = express.Router();
 
 cookieConfigRouter.get('/select-type/:type', (req, res) => {
     const { type } = req.params;
-    res.cookie('cookie-type', type).render('cookie-config/selected-type', {
+    res.cookie('cookieType', type).render('cookie-config/selected-type', {
         type,
     });
 });
--- routes/home.js
+++ routes/home.js
@@ -4,15 +4,17 @@ const { COOKIES_TYPES, COOKIES_ADDONS } = require('../data/cookies-data');
 const { handlebarsHelpers } = require('../handlebars-helpers/handlebars-helpers');
 
 homeRouter.get('/', (req, res) => {
+    const { cookieType } = req.cookies;
+    const defaultCookie = !cookieType ? 'light' : cookieType;
     const sumOrder =
-        handlebarsHelpers['find-price'](Object.entries(COOKIES_TYPES), 'light') +
+        handlebarsHelpers['find-price'](Object.entries(COOKIES_TYPES), defaultCookie) +
         ['coconut', 'strawberries', 'honey', 'candy', 'sprinkles'].reduce((prev, curr) => {
             return prev + handlebarsHelpers['find-price'](Object.entries(COOKIES_ADDONS), curr);
         }, 0);
 
     res.render('home/index', {
         cookie: {
-            type: 'light',
+            type: defaultCookie,
             addons: ['coconut', 'strawberries', 'honey', 'candy', 'sprinkles'],
         },
         types: Object.entries(COOKIES_TYPES),
