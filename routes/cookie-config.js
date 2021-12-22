597827322947bcd0c29f33bd55fa65f9d9962cfa
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-12-22 01:18:54 +0100

    create a functionality deleting single addon from cookies and updating view

--- routes/cookie-config.js
+++ routes/cookie-config.js
@@ -1,29 +1,38 @@
 const express = require('express');
 const cookieConfigRouter = express.Router();
 
-cookieConfigRouter.get('/select-type/:type', (req, res) => {
-    const { type } = req.params;
-    res.cookie('cookieType', type).render('cookie-config/selected-type', {
-        type,
+cookieConfigRouter
+    .get('/select-type/:type', (req, res) => {
+        const { type } = req.params;
+        res.cookie('cookieType', type).render('cookie-config/selected-type', {
+            type,
+        });
+    })
+    .get('/add-addon/:addon', (req, res) => {
+        const { cookieAddons } = req.cookies;
+        const { addon } = req.params;
+        const addons = cookieAddons ? JSON.parse(cookieAddons) : [];
+
+        if (!addons.includes(addon)) {
+            addons.push(addon);
+            res.cookie('cookieAddons', JSON.stringify(addons)).render('cookie-config/added-addon', {
+                addon,
+            });
+        } else {
+            res.render('cookie-config/error', {
+                addon,
+            });
+        }
+    })
+    .get('/delete-addon/:addon', (req, res) => {
+        const { cookieAddons } = req.cookies;
+        const { addon } = req.params;
+        const addons = (cookieAddons ? JSON.parse(cookieAddons) : []).filter(addonName => addonName !== addon);
+
+        res.cookie('cookieAddons', JSON.stringify(addons)).render('cookie-config/deleted-addon', {
+            addon,
+        });
     });
-});
-
-cookieConfigRouter.get('/add-addon/:addon', (req, res) => {
-    const { cookieAddons } = req.cookies;
-    const { addon } = req.params;
-    const addons = cookieAddons ? JSON.parse(cookieAddons) : [];
-
-    if (!addons.includes(addon)) {
-        addons.push(addon);
-        res.cookie('cookieAddons', JSON.stringify(addons)).render('cookie-config/added-addon', {
-            addon,
-        });
-    } else {
-        res.render('cookie-config/error', {
-            addon,
-        });
-    }
-});
 
 module.exports = {
     cookieConfigRouter,
